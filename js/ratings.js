2b723680e018d904830b2706143fa534dc577eca
Ashutosh <31060855+AshutoshJha007@users.noreply.github.com>
2022-01-26 11:13:26 +0530

    Bug Fix 1

--- js/ratings.js
+++ js/ratings.js
@@ -8,12 +8,12 @@ function collect_ratings() {
 
     let rating = 0;
     
-    const elements = document.querySelectorAll("rating");
+    const elements = document.querySelectorAll(".rating");
 
     elements.forEach(element => {
         rating = parseInt(element.id.replace("star", ""));
-        ratings.count = parseInt(element.value);
-        ratings.sum += rating * ratings.count;
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
     });
 
     if(ratings.count != 0){
