e2c9e8e088dcd1b07e8385b4ae7906f2a90014f1
Ashutosh <31060855+AshutoshJha007@users.noreply.github.com>
2022-01-26 11:06:57 +0530

    Initial Commit Average

---
+++ js/ratings.js
@@ -0,0 +1,31 @@
+function collect_ratings() {
+
+    let ratings = {
+        'count' : 0,
+        'sum' : 0,
+        'average' : 0
+    };
+
+    let rating = 0;
+    
+    const elements = document.querySelectorAll("rating");
+
+    elements.forEach(element => {
+        rating = parseInt(element.id.replace("star", ""));
+        ratings.count = parseInt(element.value);
+        ratings.sum += rating * ratings.count;
+    });
+
+    if(ratings.count != 0){
+        ratings.average = ratings.sum/count;
+    }
+
+    return ratings;
+    
+}
+
+document.addEventListener("change",() => {
+    const ratings = collect_ratings();
+    document.querySelector("average", ratings.average.toFixed(2));
+
+} );
